43b3665d459481846b0961f3819fb392f01c3bcd
Taras <matviyenkot@gmail.com>
2021-10-29 13:04:21 +0300

    v1.1 add delteBin

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ ded.classList.add('hiden');
 
         for( let i = 1; i <= localStorage.getItem('counter'); i++){
             arr.push(localStorage.getItem(i));
-            console.log(i);
+            // console.log(i);
 
             let tr = document.createElement('tr');
-            tr.innerHTML = `<td>${arr[arr.length -1]}</td>`;
-            table.append(tr);
+                //tr.setAttribute('tr-bin-to', arr.length -1);
+                tr.innerHTML = `<td style = "max-width: 250px;">${arr[arr.length -1]} </td>
+                <td class = "bins"><img src ="img/recycledBin.png" style = "width: 32px; height: 32px;" ></td>`;
+                table.append(tr);
         }
-        console.log(arr);
+        // console.log(arr);
 
 addID.addEventListener('click', ()=>{
     
@@ -45,14 +47,25 @@ addID.addEventListener('click', ()=>{
             if( input.value != 'ID already excist!!'){
                 arr.push(input.value);
 
-                arr.forEach((e, key) => {
+                arr.forEach((e) => {
                     localStorage.setItem(arr.length, e);
                     localStorage.setItem('counter', arr.length);           
                 });
         
                 let tr = document.createElement('tr');
-                tr.innerHTML = `<td>${arr[arr.length -1]}</td>`;
+                tr.setAttribute('tr-bin-to', arr.length -1);
+                tr.innerHTML = `<td style = "max-width: 250px;">${arr[arr.length -1]} </td>
+                <td class = "bins"><img src ="img/recycledBin.png" style = "width: 32px; height: 32px;" ></td>`;
                 table.append(tr);
+
+                deleteBin();
+                // const bins = document.querySelectorAll('.bins');
+
+                // bins.forEach((bin) => {
+                //     bin.addEventListener('click' , () =>{
+                //         console.log('yaa');
+                //     });
+                // });
             }
             
             output.classList.add('hiden');
@@ -72,8 +85,9 @@ addID.addEventListener('click', ()=>{
         }
         
         
-    // console.log(arr);
+    console.log(arr);
 
+    
     
 });
 
@@ -84,3 +98,21 @@ clear.addEventListener('click', ()=>{
         arr = [];
         input.value = '111';
 });
+
+deleteBin();
+
+function deleteBin(){
+    const bins = document.querySelectorAll('.bins');
+
+    bins.forEach((bin) => {
+        bin.addEventListener('click' , (e) =>{
+            console.log('yaa');
+            const binTo = e.target.getAttribute('tr-bin-to');
+            localStorage.removeItem(bins.length);
+            arr.splice(binTo, 1);
+            bin.closest("tr").remove();
+            localStorage.setItem('counter', arr.length);
+        });
+    });
+
+}
